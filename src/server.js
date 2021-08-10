3add1979d0e52fdcf217636f4080103ccdb7c96c
Rachmad Nur Hayat <rachmadnurhayat@gmail.com>
2021-08-10 14:40:28 +0700

    add users functionality

--- src/server.js
+++ src/server.js
@@ -1,12 +1,22 @@
 require('dotenv').config()
 
 const Hapi = require('@hapi/hapi')
+
+// OpenMusic Core
 const openmusic = require('./api/openmusic')
 const OpenMusicService = require('./services/postgres/OpenMusicService')
 const OpenMusicDataValidator = require('./validator/openmusic')
 
+// Users Core
+const users = require('./api/users')
+const UsersService = require('./services/postgres/UsersService')
+const UsersValidator = require('./validator/users')
+
 const init = async () => {
   const openMusicService = new OpenMusicService()
+
+  const usersService = new UsersService()
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -17,13 +27,22 @@ const init = async () => {
     }
   })
 
-  await server.register({
-    plugin: openmusic,
-    options: {
-      service: openMusicService,
-      validator: OpenMusicDataValidator
+  await server.register([
+    {
+      plugin: openmusic,
+      options: {
+        service: openMusicService,
+        validator: OpenMusicDataValidator
+      }
+    },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator
+      }
     }
-  })
+  ])
 
   await server.start()
   console.log(`Server running at ${server.info.uri}, wish u luck m8`)
